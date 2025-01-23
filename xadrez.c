67c92ff6a1f7ee649278fc2f2f891a4459ea892b
frankPro25 <frankgeorgepro2002@gmail.com>
2025-01-23 23:30:05 +0000

    xadrez v1.2.0

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,18 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void movimentoCavalo(n){
+    if(n<4){
+        if(n>2){
+            printf("Direita\n");
+        }else{
+            printf("Cima\n");
+        }
+        n++;
+        movimentoCavalo(n);
+    }
+} 
+
 int main()
 {
     // Nível Novato - Movimentação das Peças
@@ -29,6 +41,7 @@ int main()
     }
     // Nível Aventureiro - Movimentação do Cavalo
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    /*
     for (int i = 0; i < Cavalo[1]; i++)
     {
         for (int i = 0; i < Cavalo[0]; i++)
@@ -37,9 +50,11 @@ int main()
         }
         printf("Direita\n");
     }
+    */
+    
     // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    // movimentações das peças por funções recursivas.
+       movimentoCavalo(0);
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
